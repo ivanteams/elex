a39f972f21f98b804736d8c0b60bf4c10c2a6c85
Iván Rodríguez Ruiz <ivanrguez1@yahoo.es>
2024-03-20 14:29:51 +0100

    Esqueleto proyecto Elex

---
+++ src/main/java/com/soltel/elex/ElexApplication.java
@@ -0,0 +1,13 @@
+package com.soltel.elex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soltel/elex/ElexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soltel.elex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
