02b54544fee18414713f0b0657e69abf845f4c46
ivansoltel <ivan.rodriguez@soltel.es>
2024-03-22 13:56:51 +0100

    [+] Clase 2024-03-22 Proyecto Elex -> Actualizada vista Angular

--- elex_angular17/src/app/app.component.ts
+++ elex_angular17/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormulariosTiposComponent } from './formularios-tipos/formularios-tipos.component';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    FormulariosTiposComponent],
+            FormulariosTiposComponent,
+            HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'elex_angular17';
+  constructor(private http: HttpClient) {  // Inyectar HttpClient aquí
+    // Puedes usar 'http' para realizar solicitudes HTTP
+  }
 }
